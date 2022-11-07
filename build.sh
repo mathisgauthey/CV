3c4098a88b1a22347ac4989b7ffa213270918a76
Mathis Gauthey <mathis.gauthey@gmail.com>
2022-11-07 08:36:50 +0100

    resume updated: 2022-11-07 08-36-50

--- build.sh
+++
@@ -1,2 +0,0 @@
-resume export index --format html --theme jdambron-fr
-resume export index --format pdf --theme jdambron-fr
---
+++ git_global_update.sh
@@ -0,0 +1,4 @@
+git add -A && git commit -a -m "resume updated: `date +'%Y-%m-%d %H-%M-%S'`" && git push
+cd ..
+cd CV-pdf
+git add -A && git commit -a -m "resume updated: `date +'%Y-%m-%d %H-%M-%S'`" && git push
